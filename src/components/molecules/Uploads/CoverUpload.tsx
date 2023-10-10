ba01888e47bfaca57379a1bfc67d7451b21c396e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-10-10 09:14:59 +0800

    feat: add upload cover photo api route and functionality

--- src/components/molecules/Uploads/CoverUpload.tsx
+++ src/components/molecules/Uploads/CoverUpload.tsx
@@ -10,12 +10,61 @@ import toast from "react-hot-toast";
 import { uploadCoverStore } from "~/lib/stores/uploads/cover";
 import { trpc } from "~/app/_trpc/client";
 
+interface PreviewCoverImageProps {
+  imageUrl: string;
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}
+
 export default function CoverUpload() {
+  const { previewCoverImage, setImageCoverUploaded, setPreviewCoverImage } = uploadCoverStore();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleUpdateCoverImage = (e: any) => {
+    try {
+      setImageCoverUploaded(e.target.files[0]);
+
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      var allowedExtensions = /(\.jpg|\.jpeg|\.jfif|\.png)$/i;
+
+      if (e.target.value !== "" && !allowedExtensions.exec(e.target.value)) {
+        e.target.value = "";
+        setImageCoverUploaded("");
+        toast.error("Please select jpg, jpeg or png only!");
+        return;
+      }
+
+      if (e.target.files[0].size > 2097152) {
+        setImageCoverUploaded("");
+        setPreviewCoverImage("");
+        toast.error("Selected photo size exceeds 2 MB. Choose another one.");
+        return;
+      }
+
+      reader.onloadend = function () {
+        setPreviewCoverImage(reader.result);
+        setIsOpen(true);
+      };
+
+      if (file) {
+        reader.readAsDataURL(file);
+      } else {
+        setPreviewCoverImage("");
+      }
+
+      e.target.value = null;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <label
         htmlFor="uploadCover"
-        className="absolute z-20 top-3 right-3 cursor-pointer rounded-full bg-black bg-opacity-50 p-2 outline-none hover:bg-opacity-20"
+        className="absolute right-3 top-3 z-20 cursor-pointer rounded-full bg-black bg-opacity-50 p-2 outline-none hover:bg-opacity-20"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -41,9 +90,127 @@ export default function CoverUpload() {
         type="file"
         id="uploadCover"
         className="hidden"
-        onChange={() => console.log("Change Cover")}
+        onChange={handleUpdateCoverImage}
         accept=".jpg, .png, .jpeg, .jfif"
       />
+      <PreviewCoverImage imageUrl={previewCoverImage} isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
+  );
+}
+
+function PreviewCoverImage({ imageUrl, isOpen, setIsOpen }: PreviewCoverImageProps) {
+  "use client";
+
+  const router = useRouter();
+
+  const { imageCoverUploaded } = uploadCoverStore();
+
+  const utils = trpc.useContext();
+  const uploadCoverMutation = trpc.uploadCover.useMutation();
+
+  const [isPending, setIsPending] = useState<boolean>(false);
+
+  const handleUpdateCover = async () => {
+    setIsPending(true);
+
+    const coverPhoto = new FormData();
+    coverPhoto.append("image", imageCoverUploaded);
+
+    await fetch(`https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`, {
+      method: "POST",
+      body: coverPhoto,
+    })
+      .then((response) => response.json())
+      .then(async (result) => {
+        await uploadCoverMutation.mutateAsync(
+          {
+            coverUrl: result.data.url,
+          },
+          {
+            onSuccess: () => {
+              toast.success("Cover photo is updated, just wait for a moment to see the changes.");
+              utils.profile.invalidate();
+              utils.user.invalidate();
+              utils.users.invalidate();
+              router.refresh();
+              setIsPending(false);
+              setIsOpen(false);
+            },
+            onError: (error) => {
+              setIsPending(false);
+              toast.error(error.message);
+            },
+          },
+        );
+      })
+      .catch(() => {
+        toast.error("Upload image failed, try again.");
+      });
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={() => !isPending && setIsOpen(false)} className="relative z-50">
+      <div className="fixed inset-0 bg-black/50 backdrop-blur-sm" aria-hidden="true" />
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <Dialog.Panel className="mx-auto flex h-auto w-full max-w-3xl flex-col gap-y-5 overflow-y-auto rounded-lg bg-white p-5">
+          <div className="flex w-full flex-row items-center justify-between">
+            <p>Change Cover</p>
+            {!isPending && (
+              <button
+                type="button"
+                className="outline-none hover:opacity-50"
+                onClick={() => setIsOpen(false)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            )}
+          </div>
+          <div className="flex w-full flex-col items-center gap-y-3">
+            <Image
+              className="h-[10rem] w-full object-cover md:h-[30rem]"
+              src={imageUrl}
+              alt="cover-image"
+              width={500}
+              height={500}
+              quality={100}
+              placeholder="blur"
+              blurDataURL={imageUrl}
+            />
+            <p className="text-xs">This will be the actual display of your cover photo.</p>
+          </div>
+          <div className="items center flex w-full flex-row gap-x-2">
+            <button
+              disabled={isPending}
+              type="button"
+              className={clsx(
+                isPending && "cursor-not-allowed",
+                "custom-button w-full outline-none",
+              )}
+              onClick={handleUpdateCover}
+            >
+              {isPending ? "Saving..." : "Save"}
+            </button>
+            {!isPending && (
+              <button
+                type="button"
+                className="custom-button-outlined w-full outline-none"
+                onClick={() => setIsOpen(false)}
+              >
+                Cancel
+              </button>
+            )}
+          </div>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
   );
 }

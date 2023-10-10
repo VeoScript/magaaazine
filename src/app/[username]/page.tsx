a27295d5c0c2304eff3330b849614458a2d6997d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-10-10 16:44:37 +0800

    feat: add social media holder component (#15)

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import Image from "next/image";
 
 import getBase64 from "~/lib/functions/getBase64";
+import SocialMediaHolder from "~/components/molecules/SocialMediaHolder";
 import ChatBox from "~/components/molecules/ChatBox";
 import ProfileUpload from "~/components/molecules/Uploads/ProfileUpload";
 import CoverUpload from "~/components/molecules/Uploads/CoverUpload";
@@ -56,9 +57,9 @@ export default async function UserPage({ params }: { params: { username: string 
                   blurDataURL={await getBase64(profile?.cover_photo)}
                 />
               </div>
-              {user?.id === profile.id && <CoverUpload />}
             </>
           )}
+          {user?.id === profile.id && <CoverUpload />}
           <div
             className={clsx(
               profile?.cover_photo ? "text-white" : "text-black",
@@ -66,8 +67,8 @@ export default async function UserPage({ params }: { params: { username: string 
             )}
           >
             <div className="flex w-full max-w-xl flex-col items-center gap-y-5">
-              {profile?.profile_photo ? (
-                <div className="relative">
+              <div className="relative">
+                {profile?.profile_photo ? (
                   <Image
                     className="h-[13rem] w-[13rem] rounded-full object-cover"
                     src={profile?.profile_photo}
@@ -79,27 +80,27 @@ export default async function UserPage({ params }: { params: { username: string 
                     placeholder="blur"
                     blurDataURL={await getBase64(profile?.profile_photo)}
                   />
-                  {user?.id === profile.id && <ProfileUpload />}
-                </div>
-              ) : (
-                <div className="flex h-[13rem] w-[13rem] flex-row items-center justify-center rounded-full bg-neutral-300 object-cover">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-[8rem] w-[8rem] text-white"
-                  >
-                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                </div>
-              )}
+                ) : (
+                  <div className="flex h-[13rem] w-[13rem] flex-row items-center justify-center rounded-full bg-neutral-300 object-cover">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="h-[8rem] w-[8rem] text-white"
+                    >
+                      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                )}
+                {user?.id === profile.id && <ProfileUpload />}
+              </div>
               <div className="flex w-full flex-col items-center gap-y-3">
                 <div className="flex w-full flex-col items-center">
                   <h1 className="text-2xl font-bold">
@@ -109,8 +110,30 @@ export default async function UserPage({ params }: { params: { username: string 
                     <h2 className="text-base font-light">@{profile?.username}</h2>
                   )}
                 </div>
+                {(profile.facebook_link ||
+                  profile.instagram_link ||
+                  profile.twitterx_link ||
+                  profile.linkedin_link ||
+                  profile.github_link ||
+                  profile.website_link) && (
+                  <SocialMediaHolder
+                    facebook_link={profile.facebook_link}
+                    instagram_link={profile.instagram_link}
+                    twitterx_link={profile.twitterx_link}
+                    linkedin_link={profile.linkedin_link}
+                    github_link={profile.github_link}
+                    website_link={profile.website_link}
+                  />
+                )}
                 {profile?.short_bio && (
-                  <p className="text-lg text-neutral-300">{profile?.short_bio}</p>
+                  <p
+                    className={clsx(
+                      profile.cover_photo ? "text-neutral-300" : "text-neutral-500",
+                      "text-center text-lg",
+                    )}
+                  >
+                    {profile?.short_bio}
+                  </p>
                 )}
               </div>
               {profile?.favorite_quote && (
---
+++ src/components/molecules/SocialMediaHolder.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+
+import facebook from '../../../public/svg/facebook.svg';
+import instagram from '../../../public/svg/instagram.svg';
+import twitter from '../../../public/svg/twitter.svg';
+import linkedin from '../../../public/svg/linkedin.svg';
+import github from '../../../public/svg/github.svg';
+
+interface SocialMediaHolderProps {
+  facebook_link: string | null;
+  instagram_link: string | null;
+  twitterx_link: string | null;
+  linkedin_link: string | null;
+  github_link: string | null;
+  website_link: string | null;
+}
+
+export default function SocialMediaHolder({
+  facebook_link,
+  instagram_link,
+  twitterx_link,
+  linkedin_link,
+  github_link,
+  website_link,
+}: SocialMediaHolderProps) {
+  return (
+    <div className="my-3 flex flex-row items-center gap-x-3">
+      {facebook_link && (
+        <Link
+          href={`https://www.facebook.com/${facebook_link}`}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="Facebook"
+        >
+          <Image
+            priority
+            src={facebook}
+            alt="facebook"
+            width={100}
+            height={100}
+            className="h-6 w-6"
+          />
+        </Link>
+      )}
+      {instagram_link && (
+        <Link
+          href={`https://www.instagram.com/${instagram_link}`}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="Instagram"
+        >
+          <Image
+            priority
+            src={instagram}
+            alt="instagram"
+            width={100}
+            height={100}
+            className="h-6 w-6"
+          />
+        </Link>
+      )}
+      {twitterx_link && (
+        <Link
+          href={`https://twitter.com/${twitterx_link}`}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="Twitter/X"
+        >
+          <Image
+            priority
+            src={twitter}
+            alt="twitter"
+            width={100}
+            height={100}
+            className="h-6 w-6"
+          />
+        </Link>
+      )}
+      {linkedin_link && (
+        <Link
+          href={`https://www.linkedin.com/in/${linkedin_link}`}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="LinkedIn"
+        >
+          <Image
+            priority
+            src={linkedin}
+            alt="linkedin"
+            width={100}
+            height={100}
+            className="h-6 w-6"
+          />
+        </Link>
+      )}
+      {github_link && (
+        <Link
+          href={`https://github.com/${github_link}`}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="Github"
+        >
+          <Image priority src={github} alt="github" width={100} height={100} className="h-6 w-6" />
+        </Link>
+      )}
+      {website_link && (
+        <Link
+          href={website_link}
+          target="_blank"
+          data-tooltip-id="onlyself-tooltip"
+          data-tooltip-content="Website"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="h-6 w-6 text-white"
+          >
+            <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+            <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+          </svg>
+        </Link>
+      )}
+    </div>
+  );
+}

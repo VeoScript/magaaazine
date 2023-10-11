fce1473894ef2314c0293a9d96b7c3b0c5c55b72
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-10-11 16:34:05 +0800

    chore: messages and files page redirect to sign in if not authenticated

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,8 +1,13 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import MessagesList from "~/components/molecules/Lists/MessagesList";
 
 import { serverClient } from "../_trpc/serverClient";
 
 export default async function Messages() {
+  if (!cookies().has(`${process.env.COOKIE_NAME}`)) redirect("/signin");
+
   const userData = await serverClient.user();
   const messagesData = await serverClient.messages({
     cursor: "",

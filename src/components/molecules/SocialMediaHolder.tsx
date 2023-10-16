c416abaaa141cfe1b5fa3af22d86491e5a69a7b1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-10-16 21:22:35 +0800

    chore: update link svg icon color for white background

--- src/components/molecules/SocialMediaHolder.tsx
+++ src/components/molecules/SocialMediaHolder.tsx
@@ -123,7 +123,7 @@ export default function SocialMediaHolder({
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="h-6 w-6 text-white"
+            className="h-6 w-6 text-neutral-400"
           >
             <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />

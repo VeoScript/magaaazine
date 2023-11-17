03d30fa143d50cddeabbd4d214cb5b5ff34d16db
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-11-17 15:41:11 +0800

    feat: update trpc-version

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -5,10 +5,7 @@ import { appRouter } from "~/server";
 export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
-      url:
-        process.env.NODE_ENV === "development"
-          ? `${process.env.DEV_URL}/api/trpc`
-          : `${process.env.PROD_URL}/api/trpc`,
+      url: "/api/trpc",
     }),
   ],
 });
